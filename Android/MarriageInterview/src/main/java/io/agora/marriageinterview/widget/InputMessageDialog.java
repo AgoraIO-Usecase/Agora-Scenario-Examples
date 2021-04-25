1525dd2651538b7112137f4291b5fc3439ba1991
chenhengfei <chenhengfei@agora.io>
2021-04-25 16:53:39 +0800

    底部菜单

--- Android/MarriageInterview/src/main/java/io/agora/marriageinterview/widget/InputMessageDialog.java
+++ Android/MarriageInterview/src/main/java/io/agora/marriageinterview/widget/InputMessageDialog.java
@@ -2,12 +2,7 @@ package io.agora.marriageinterview.widget;
 
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,7 +12,7 @@ import com.agora.data.BaseError;
 import com.agora.data.manager.RTMManager;
 import com.agora.data.observer.DataCompletableObserver;
 
-import io.agora.baselibrary.base.DataBindBaseDialog;
+import io.agora.baselibrary.base.DataBindBottomSheetDialogFragment;
 import io.agora.marriageinterview.R;
 import io.agora.marriageinterview.databinding.DialogInputMessageBinding;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -27,26 +22,13 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
  *
  * @author chenhengfei@agora.io
  */
-public class InputMessageDialog extends DataBindBaseDialog<DialogInputMessageBinding> implements View.OnClickListener {
+public class InputMessageDialog extends DataBindBottomSheetDialogFragment<DialogInputMessageBinding> implements View.OnClickListener {
     private static final String TAG = InputMessageDialog.class.getSimpleName();
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        Window win = getDialog().getWindow();
-        WindowManager.LayoutParams params = win.getAttributes();
-        params.gravity = Gravity.BOTTOM;
-        params.width = ViewGroup.LayoutParams.MATCH_PARENT;
-        params.height = ViewGroup.LayoutParams.WRAP_CONTENT;
-        win.setAttributes(params);
-        return super.onCreateView(inflater, container, savedInstanceState);
-    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setStyle(STYLE_NORMAL, R.style.Dialog_Bottom);
+        setStyle(STYLE_NORMAL, R.style.Dialog_Input);
     }
 
     @Override
