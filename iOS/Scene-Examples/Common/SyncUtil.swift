86a791cf414d89b5eddec487b03620922c2fe1a9
zhaoyongqiang <zhaoyongqiang@agora.io>
2021-11-22 16:52:34 +0800

    修改sync封装结构

--- iOS/Scene-Examples/Common/SyncUtil.swift
+++ iOS/Scene-Examples/Common/SyncUtil.swift
@@ -11,7 +11,7 @@ class SyncUtil: NSObject {
     private static var manager: SyncManager?
     private override init() { }
     private static var subscribeCollectionDelegate: ISyncManagerLiveQuery?
-    private static var sceneRefs: [SceneReference] = [SceneReference]()
+    private static var sceneRefs: [String: SceneReference] = [String: SceneReference]()
     
     static func initSyncManager(sceneId: String) {
         let dict = [SYNC_MANAGER_PARAM_KEY_APPID: KeyCenter.AppId,
@@ -28,8 +28,7 @@ class SyncUtil: NSObject {
         let params = JSONObject.toDictionary(jsonStr: jsonString)
         let scene = Scene(id: id, userId: userId, property: params)
         let sceneRef = manager.joinScene(with: scene, delegate: delegate)
-        guard !sceneRefs.contains(where: { $0.id == id }) else { return }
-        sceneRefs.append(sceneRef)
+        sceneRefs[id] = sceneRef
     }
     
     class func fetchAll(delegate: IObjectListDelegate) {
@@ -39,7 +38,7 @@ class SyncUtil: NSObject {
     class func fetchCollection(id: String,
                                className: String,
                                delegate: IObjectListDelegate) {
-        let sceneRef = sceneRefs.filter({ $0.id == id }).first
+        let sceneRef = sceneRefs[id]
         sceneRef?.collection(className: className).get(delegate: delegate)
     }
     
@@ -47,7 +46,7 @@ class SyncUtil: NSObject {
                              className: String,
                              params: [String: Any],
                              delegate: IObjectDelegate?) {
-        let sceneRef = sceneRefs.filter({ $0.id == id }).first
+        let sceneRef = sceneRefs[id]
         sceneRef?.collection(className: className)
             .add(data: params, delegate: delegate)
     }
@@ -57,7 +56,7 @@ class SyncUtil: NSObject {
                                 objectId: String,
                                 params: [String: Any],
                                 delegate: IObjectDelegate?) {
-        let sceneRef = sceneRefs.filter({ $0.id == id }).first
+        let sceneRef = sceneRefs[id]
         sceneRef?.collection(className: className).document(id: objectId).update(data: params, delegate: delegate)
     }
     
@@ -65,7 +64,7 @@ class SyncUtil: NSObject {
                                    className: String,
                                    documentId: String? = nil,
                                    delegate: ISyncManagerEventDelegate) {
-        let sceneRef = sceneRefs.filter({ $0.id == id }).first
+        let sceneRef = sceneRefs[id]
         if documentId == nil {
             subscribeCollectionDelegate = sceneRef?.collection(className: className)
                 .document()
@@ -78,29 +77,28 @@ class SyncUtil: NSObject {
     }
     
     class func unsubscribe(id: String, className: String) {
-        let sceneRef = sceneRefs.filter({ $0.id == id }).first
+        let sceneRef = sceneRefs[id]
         sceneRef?.collection(className: className).document().unsubscribe()
     }
     
     class func leaveScene(id: String) {
-        guard let index = sceneRefs.firstIndex(where: { $0.id == id }) else { return }
-        sceneRefs.remove(at: index)
+        sceneRefs.removeValue(forKey: id)
     }
     
     class func deleteDocument(id: String,
-                                className: String,
-                                objectId: String?,
-                                delegate: IDocumentReferenceDelegate?) {
-        guard let sceneRef = sceneRefs.filter({ $0.id == id }).first else { return }
+                              className: String,
+                              objectId: String?,
+                              delegate: IDocumentReferenceDelegate?) {
+        let sceneRef = sceneRefs[id]
         /// 删除单条数据
-        sceneRef.collection(className: className).document(id: objectId ?? "").delete(delegate: delegate)
+        sceneRef?.collection(className: className).document(id: objectId ?? "").delete(delegate: delegate)
     }
     
     class func deleteCollection(id: String,
                                 className: String,
                                 delegate: IDocumentReferenceDelegate?) {
-        guard let sceneRef = sceneRefs.filter({ $0.id == id }).first else { return }
+        let sceneRef = sceneRefs[id]
         /// 删除数据
-        sceneRef.collection(className: className).delete(delegate: delegate)
+        sceneRef?.collection(className: className).delete(delegate: delegate)
     }
 }
