2f56f83540031d38679018ccf91363bb4ee19317
zhaoyongqiang <zhaoyongqiang@agora.io>
2021-12-17 18:48:48 +0800

    fix 屏幕尺寸显示问题

--- iOS/Scene-Examples/Game/Models/GameInfoModel.swift
+++ iOS/Scene-Examples/Game/Models/GameInfoModel.swift
@@ -8,11 +8,11 @@
 import UIKit
 
 struct GameInfoModel: Codable {
-    var objectId: String = ""
+    var objectId: String? = ""
     
     var status: GameStatus = .no_start
     
-    var gameUid: String = ""
+    var gameUid: String? = ""
     
     var gameId: GameCenterType? = .you_draw_i_guess
 }
