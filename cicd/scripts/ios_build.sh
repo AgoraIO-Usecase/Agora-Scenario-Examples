f1cd66fa09f181363e642cac18ff6f599589db95
xucz <xgfd3@users.noreply.github.com>
2022-10-11 18:30:38 +0800

    [iOS]fix azure ci 2.

--- cicd/scripts/ios_build.sh
+++ cicd/scripts/ios_build.sh
@@ -26,37 +26,27 @@ echo PBXPROJ_PATH: $PBXPROJ_PATH
 
 # 主项目工程配置
 # Debug
-/usr/libexec/PlistBuddy -c "Set :objects:E79C25ED273B67DD00909CD4:buildSettings:CODE_SIGN_STYLE 'Manual'" $PBXPROJ_PATH
-/usr/libexec/PlistBuddy -c "Set :objects:E79C25ED273B67DD00909CD4:buildSettings:DEVELOPMENT_TEAM 'GM72UGLGZW'" $PBXPROJ_PATH
-/usr/libexec/PlistBuddy -c "Set :objects:E79C25ED273B67DD00909CD4:buildSettings:PROVISIONING_PROFILE_SPECIFIER 'App'" $PBXPROJ_PATH
+/usr/libexec/PlistBuddy -c "Set :objects:E79C25F0273B67DD00909CD4:buildSettings:CODE_SIGN_STYLE 'Manual'" "$PBXPROJ_PATH"
+/usr/libexec/PlistBuddy -c "Set :objects:E79C25F0273B67DD00909CD4:buildSettings:DEVELOPMENT_TEAM 'GM72UGLGZW'" "$PBXPROJ_PATH"
+/usr/libexec/PlistBuddy -c "Set :objects:E79C25F0273B67DD00909CD4:buildSettings:PROVISIONING_PROFILE_SPECIFIER 'App'" "$PBXPROJ_PATH"
 # Release
-/usr/libexec/PlistBuddy -c "Set :objects:E79C25EE273B67DD00909CD4:buildSettings:CODE_SIGN_STYLE 'Manual'" $PBXPROJ_PATH
-/usr/libexec/PlistBuddy -c "Set :objects:E79C25EE273B67DD00909CD4:buildSettings:DEVELOPMENT_TEAM 'GM72UGLGZW'" $PBXPROJ_PATH
-/usr/libexec/PlistBuddy -c "Set :objects:E79C25EE273B67DD00909CD4:buildSettings:PROVISIONING_PROFILE_SPECIFIER 'App'" $PBXPROJ_PATH
-
-
-# Debug
-/usr/libexec/PlistBuddy -c "Set :objects:E79C25F0273B67DD00909CD4:buildSettings:CODE_SIGN_STYLE 'Manual'" $PBXPROJ_PATH
-/usr/libexec/PlistBuddy -c "Set :objects:E79C25F0273B67DD00909CD4:buildSettings:DEVELOPMENT_TEAM 'GM72UGLGZW'" $PBXPROJ_PATH
-/usr/libexec/PlistBuddy -c "Set :objects:E79C25F0273B67DD00909CD4:buildSettings:PROVISIONING_PROFILE_SPECIFIER 'App'" $PBXPROJ_PATH
-# Release
-/usr/libexec/PlistBuddy -c "Set :objects:E79C25F1273B67DD00909CD4:buildSettings:CODE_SIGN_STYLE 'Manual'" $PBXPROJ_PATH
-/usr/libexec/PlistBuddy -c "Set :objects:E79C25F1273B67DD00909CD4:buildSettings:DEVELOPMENT_TEAM 'GM72UGLGZW'" $PBXPROJ_PATH
-/usr/libexec/PlistBuddy -c "Set :objects:E79C25F1273B67DD00909CD4:buildSettings:PROVISIONING_PROFILE_SPECIFIER 'App'" $PBXPROJ_PATH
+/usr/libexec/PlistBuddy -c "Set :objects:E79C25F1273B67DD00909CD4:buildSettings:CODE_SIGN_STYLE 'Manual'" "$PBXPROJ_PATH"
+/usr/libexec/PlistBuddy -c "Set :objects:E79C25F1273B67DD00909CD4:buildSettings:DEVELOPMENT_TEAM 'GM72UGLGZW'" "$PBXPROJ_PATH"
+/usr/libexec/PlistBuddy -c "Set :objects:E79C25F1273B67DD00909CD4:buildSettings:PROVISIONING_PROFILE_SPECIFIER 'App'" "$PBXPROJ_PATH"
 
 TARGET_FILE=""
 if [ ! -f "Podfile" ];then
 TARGET_FILE="${APP_Project}.xcodeproj"
-xcodebuild clean -project ${TARGET_FILE} -scheme "${APP_TARGET}" -configuration ${MODE}
-xcodebuild -project ${TARGET_FILE} -scheme "${APP_TARGET}" -configuration ${MODE} -archivePath ${ArchivePath} archive
+xcodebuild clean -project "${TARGET_FILE}" -scheme "${APP_TARGET}" -configuration ${MODE}
+xcodebuild -project "${TARGET_FILE}" -scheme "${APP_TARGET}" -configuration ${MODE} -archivePath "${ArchivePath}" archive
 else
 pod install
 TARGET_FILE="${APP_Project}.xcworkspace"
-xcodebuild clean -workspace ${TARGET_FILE} -scheme "${APP_TARGET}" -configuration ${MODE}
-xcodebuild archive -workspace ${TARGET_FILE} -scheme "${APP_TARGET}" -configuration ${MODE} -archivePath ${ArchivePath} -destination 'generic/platform=iOS'
+xcodebuild clean -workspace "${TARGET_FILE}" -scheme "${APP_TARGET}" -configuration ${MODE}
+xcodebuild archive -workspace "${TARGET_FILE}" -scheme "${APP_TARGET}" -configuration ${MODE} -archivePath "${ArchivePath}" -destination 'generic/platform=iOS'
 fi
 
-xcodebuild -exportArchive -exportOptionsPlist ${Export_Plist_File} -archivePath ${ArchivePath} -exportPath .
+xcodebuild -exportArchive -exportOptionsPlist ${Export_Plist_File} -archivePath "${ArchivePath}" -exportPath .
 
 mkdir app
 mv *.ipa app && mv *.xcarchive app
